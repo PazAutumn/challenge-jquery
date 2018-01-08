685f0a56beb2b965440f3374dc89a8848251ae2e
pazautumn <pazcontrerastello@gmail.com>
2018-01-08 17:00:34 -0300

    Etapa 7: asignando función a los botones recipe y make it

--- assets/js/script-recipe.js
+++ assets/js/script-recipe.js
@@ -4,4 +4,15 @@ $(document).ready( function(){
 	*Escondiendo icono de menú
 	*/
 	$('.js-menu').hide();
+
+	/*
+	*asignando función a los botones recipe y make it
+	*/
+	$('.js-show-recipe').click(function(){
+		$('.page').removeClass('make');
+	})
+
+	$('.js-show-make').click(function(){
+		$('.page').addClass('make');
+	})
 });
